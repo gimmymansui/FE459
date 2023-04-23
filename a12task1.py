934aab58e7dade85743f12f12d71db4945adff6c
gimmymansui <114517092+gimmymansui@users.noreply.github.com>
2023-04-23 16:57:01 -0400

    Update a12task1.py

--- a12task1.py
+++ a12task1.py
@@ -8,17 +8,15 @@ import pandas as pd
 import matplotlib as plt
 
 def create_bollinger_bands(df, window=21, no_of_std=1, column_name=''):
+    '''Creates bollinger bands using dataframes from pandas'''
     if not column_name:
         column_name = df.columns[0]
-    # Calculate the rolling mean and standard deviation
     rolling_mean = df[column_name].rolling(window=window).mean()
     rolling_std = df[column_name].rolling(window=window).std()
     
-    # Calculate the upper and lower Bollinger Bands
     upper_bound = rolling_mean + (rolling_std * no_of_std)
     lower_bound = rolling_mean - (rolling_std * no_of_std)
     
-    # Create a new DataFrame to store the results
     bollinger_bands = pd.DataFrame(index=df.index)
     bollinger_bands['Observation'] = df[column_name]
     bollinger_bands['RollingMean'] = rolling_mean
@@ -28,41 +26,34 @@ def create_bollinger_bands(df, window=21, no_of_std=1, column_name=''):
     return bollinger_bands
 
 def create_long_short_position(df):
+    '''creates long/short positions'''
     position_df = pd.DataFrame(index=df.index)
-    # Initialize the Position column to 0
+    # makes position column to 0
     position_df['Position'] = 0
     
-    # Loop through the DataFrame and determine the position
+    # loops through the df and determines the position
     for i in range(1, len(df)):
-        # Create a long position when the Observation crosses above the UpperBound
+        #makes long short 1 , -1 respectively
         if df['Observation'][i] > df['UpperBound'][i] and df['Observation'][i - 1] <= df['UpperBound'][i - 1]:
             position_df['Position'][i] = 1
-        # Create a short position when the Observation crosses below the LowerBound
         elif df['Observation'][i] < df['LowerBound'][i] and df['Observation'][i - 1] >= df['LowerBound'][i - 1]:
             position_df['Position'][i] = -1
-        # Hold the previous position
         else:
             position_df['Position'][i] = position_df['Position'][i - 1]
     
     return position_df
 
 def calculate_long_short_returns(df, position, column_name=''):
+    '''calculates long_short_returns'''
     if not column_name:
         column_name = df.columns[0]
     
-        # Calculate the daily returns
     daily_returns = df[column_name].pct_change()
-    
-    # Calculate the market returns
     market_return = daily_returns
-    
-    # Calculate the strategy returns
     strategy_return = daily_returns * position['Position'].shift(1)
     
-    # Calculate the abnormal returns
-    abnormal_return = strategy_return - market_return
-    
-    # Create a new DataFrame to store the results
+    abnormal_return = strategy_return - market_return #calcs the difference
+
     returns_df = pd.DataFrame(index=df.index)
     returns_df['Market Return'] = market_return
     returns_df['Strategy Return'] = strategy_return
@@ -72,7 +63,8 @@ def calculate_long_short_returns(df, position, column_name=''):
 
 
 def plot_cumulative_returns(df):
-    # Calculate cumulative returns for each column
+    '''plots the cum return of the input dataframe'''
+    # calculate cumulative returns for each column
     cumulative_returns = (1 + df).cumprod()
     cumulative_returns.plot()
     plt.xlabel('Date')
